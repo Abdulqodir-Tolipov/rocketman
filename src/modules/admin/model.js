7a2b94f6bff35e1e805ba32a1ef4c3002e79554c
Asadulloh <brshahzod@gmail.com>
2021-10-27 15:33:53 +0500

    added admin rout

--- src/modules/admin/model.js
+++ src/modules/admin/model.js
@@ -1,12 +1,73 @@
-const fetch = require("../../library/postgres")
+const fetch = require('../../library/postgres.js')
 
-const get_admin = `select * from admins`
+const get = `
+    SELECT * FROM admins
+`
 
-const get = async (req, res) => {
-    return res.json(await fetch(get_admin))
-}
+const post = `
+    INSERT INTO admins (username, password) values ($1, $2)
+    RETURNING *
+`
+
+const put = `
+WITH old_data as (
+    SELECT
+    user_id,
+    username,
+    password
+    FROM admins
+    WHERE user_id = $1
+) UPDATE admins a SET
+    username = (
+        CASE
+            WHEN length($2) > 1 THEN $2
+            ELSE o.username
+        END),
+    password = (
+        CASE 
+            WHEN length($3) > 1 THEN $3
+            ELSE o.password
+        END)
+FROM old_data o
+WHERE a.user_id = $1
+RETURNING a.*
+`
+
+const delet = `
+    DELETE FROM admins where user_id = $1
+    RETURNING *
+`
 
 module.exports = {
-    get
-}
+    get: async(_, res) => {
+        try {
+            return res.json(await fetch(get))
+        } catch (error) {
+            throw error
+        }
+    },
 
+    post: async({username, password}) => {
+        try {
+            return await fetch(post, username, password)            
+        } catch (error) {
+            throw error
+        }
+    },
+
+    put: async({user_id, username, password}) => {
+        try {
+            return await fetch(put, user_id, username, password)
+        } catch (error) {
+            throw error
+        }
+    },
+
+    delete: async({user_id}) => {
+        try {
+            return await fetch(delet, user_id)
+        } catch (error) {
+            throw error
+        }
+    }
+}
--- src/modules/category/model.js
+++ src/modules/category/model.js
@@ -1,6 +1,5 @@
 // putiga so`roq bormagan ya`ni put qilinmagan
 
-
 const fetch = require("../../library/postgres")
 
 const get = `select * from category`
@@ -8,15 +7,32 @@ const get = `select * from category`
 const post = `
 insert into category(category_name, category_status) values ($1, $2)`
 
-
 const put =
-    `update  category 
-    set 
-        category_name = $2,
-        category_status = $3
-    where category_id = $1 
-    returning * `
-
+    `
+    with old_data as(
+        select
+        category_id,
+        category_name,
+        category_status
+        from category
+        where category_id = $1
+    ) update category set
+        category_name = (
+            case
+                when length($2) > 1 THEN $2
+                else old_data.category_name
+            end
+        ),
+        category_status = (
+            case
+                when $3 <> old_data.category_status THEN $3
+                else old_data.category_status
+            end
+        )
+    from old_data
+    where category.category_id = $1
+    returning *
+    `
 
 const delet = `
 DELETE FROM category
@@ -24,19 +40,36 @@ where category_id=$1
 returning *`
 
 
-
 module.exports = {
     get: async (_, res) => {
-        return res.json(await fetch(get))
+        try {
+            return res.json(await fetch(get))
+        } catch (error) {
+            throw error
+        }
     },
-    post: ({ category_name, category_status }) => {
-        return fetch(post, category_name, category_status)
 
+    post: async ({ category_name, category_status }) => {
+        try {
+            return await fetch(post, category_name, category_status)
+        } catch (error) {
+            throw error
+        }
     },
+
     put: async ({ category_id, category_name, category_status }) => {
-        return await fetch(put, category_id, category_name, category_status)
+        try {
+            return await fetch(put, category_id, category_name, category_status)
+        } catch (error) {
+            throw error
+        }
     },
+    
     delete: async ({ category_id }) => {
-        return await fetch(delet, category_id)
+        try {
+            return await fetch(delet, category_id)
+        } catch (error) {
+            throw error
+        }
     }
 }
