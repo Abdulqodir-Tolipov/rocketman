dfbccb9b7e9a20f2f956874dcf761d2d599df046
Gaybulla <fayzullayevgn@gmail.com>
2021-10-28 15:41:15 +0500

    bug fixed

--- src/modules/subcategory/controller.js
+++ src/modules/subcategory/controller.js
@@ -14,7 +14,7 @@ module.exports = {
             }
         } catch (error) {
             return res.status(403).json({
-                stgit@github.com:Abdulqodir-Tolipov/rocketman.gitatus: 403,
+                status: 403,
                 message: "Something wrong..."
             })
         }
