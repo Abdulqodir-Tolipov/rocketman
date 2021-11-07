8dae6a9c8973ba2d01558334e159a5f162715de9
Abdulqodir-Tolipov <tolipovabdulqodir@gmail.com>
2021-11-07 14:18:29 +0500

    cookie is updated

--- middlewares/acceesMidlvware.js
+++ middlewares/acceesMidlvware.js
@@ -4,7 +4,7 @@ function access(req, res, next) {
 
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,token');
 
    res.setHeader('Access-Control-Allow-Credentials', true);
 
--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -4,9 +4,12 @@ function validateCookie(req, res, next) {
   try {
     if (req.url == '/login') return next();
     if (!req.cookies.token) throw new Error('Token required');
+    if (!req.headers.token) throw new Error('Token required');
     const isAdmin = verify(req.cookies.token);
+    const front = verify(req.headers.token);
 
     if (isAdmin == 'admin' || isAdmin == 'superadmin') next();
+    if (front == 'admin' || front == 'superadmin') next();
     else throw new Error('Forbidden');
   } catch (error) {
     res.status(500).json({
