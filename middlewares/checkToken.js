227e431530f4375e2ac1a912d2c0cf8f1aa84761
Abdulqodir-Tolipov <tolipovabdulqodir@gmail.com>
2021-11-09 16:06:37 +0500

    token updated

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -3,13 +3,11 @@ const { verify } = require('../utils/jwt.js');
 function validateCookie(req, res, next) {
   try {
     if (req.url === '/login') return next();
-    if (!req.headers.token && req.cookies.token) throw new Error('Token required');
-    const front = verify(req.headers.token);
-    if (front === 'admin' || front === 'superadmin') return next();
-    if (!req.cookies.token) throw new Error('Token required');
-    const isAdmin = verify(req.cookies.token);
-    if (isAdmin === 'admin' || isAdmin === 'superadmin') return next();
-    else throw new Error('Forbidden')
+    if (req.headers.token || req.cookies.token) {
+      let isAdmin = verify(req.headers.token || req.cookies.token);
+      if (isAdmin === 'admin' || isAdmin === 'superadmin') return next();
+      else throw new Error('Forbidden');
+    } else throw new Error('token rquired');
   } catch (error) {
     res.status(500).json({
       status: 500,
