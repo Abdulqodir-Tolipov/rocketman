974f8f4d11b483f6e36cc2972bc6973f99ea2c9a
shaxi007 <teshayevshaxboz77@gmail.com>
2021-11-09 12:49:53 +0500

    updated

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -3,14 +3,13 @@ const { verify } = require('../utils/jwt.js');
 function validateCookie(req, res, next) {
   try {
     console.log(req.cookies.token,req.headers.token)
-    if (req.url == '/login') return next();
-    else if (!req.cookies.token) throw new Error('Token required');
-    else if (!req.headers.token) throw new Error('Token required');
+    if (req.url === '/login') return next();
+    else if (!req.cookies.token || !req.headers.token) throw new Error('Token required');
     const isAdmin = verify(req.cookies.token);
     const front = verify(req.headers.token);
     console.log(front)
-    if (isAdmin == 'admin' || isAdmin == 'superadmin') next();
-    else if (front == 'admin' || front == 'superadmin') next();
+    if (isAdmin === 'admin' || isAdmin === 'superadmin') next();
+    else if (front === 'admin' || front === 'superadmin') next();
     else throw new Error('Forbidden');
   } catch (error) {
     console.log(error)
